eb2049e35a21eaca057ffa6d0f6be9f6be9d0971
tetsuya-stn <t.hitotsuishi@gmail.com>
2024-04-27 08:55:07 +0900

    Boardを9x9のマス形式で表示するprintBoardを実装

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
 
 // Boardは数独の9x9のマスを示す
 type Board [9][9]int
+
+// Boardを9x9のマス形式で表示する
+func printBoard(b Board) string {
+	var buf bytes.Buffer
+	for i := 0; i < 9; i++ {
+		if i%3 == 0 {
+			buf.WriteString("+---+---+---+\n")
+		}
+		for j := 0; j < 9; j++ {
+			if j%3 == 0 {
+				buf.WriteString("|")
+			}
+			buf.WriteString(strconv.Itoa((b[i][j])))
+		}
+		buf.WriteString("|\n")
+	}
+	buf.WriteString("+---+---+---+\n")
+
+	return buf.String()
+}
 
 func main() {
 	b := Board{
@@ -17,5 +41,5 @@ func main() {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 	}
-	fmt.Printf("b = %v\n", b)
+	fmt.Printf("%v\n", printBoard(b))
 }
